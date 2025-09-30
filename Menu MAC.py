83d581116e955738010bdc1cf8f9627cfdeb69eb
JonathamAraujo <159147583+JonathamAraujo@users.noreply.github.com>
2025-09-30 11:15:58 -0300

    Add files via upload

---
+++ Menu MAC.py
@@ -0,0 +1,16 @@
+def titulo():
+    print("-"*30)
+    print("     LABIRINTO DE COBRINHA       ")
+    print("-"*30)
+    return 
+titulo()
+
+def inicio():
+    print("-"*30)
+    print("             JOGAR          ")
+    print("-"*30)
+    return
+inicio()
+nivel=input("Selecione a Dificuldade (Fácil/Médio/Difícil): ").upper()
+
+
